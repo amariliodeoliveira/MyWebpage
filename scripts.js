c5e9a09bb2742af00f7fb953b7f78f9cabc80773
Amarilio de Oliveira <amarilioalencar@gmail.com>
2022-10-02 05:02:54 -0300

    modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,18 +1,29 @@
-/* Modal Config */
-const openModalButton1 = document.querySelector("#aboutMeModal");
-const openModalButton2 = document.querySelector("#portfolioModal");
-const closeModalButton = document.querySelector("#closeModal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fadeModal");
+/* About Me | Modal */
+const openAboutMe = document.querySelector("#openAboutMe");
+const closeAboutMe = document.querySelector("#closeAboutMe");
+const aboutMe = document.querySelector("#aboutMe");
+const fadeAboutMe = document.querySelector("#fadeAboutMe");
 
-const toggleModal = () => {
-  modal.classList.toggle("hideModal");
-  fade.classList.toggle("hideModal");
+const toggleAboutMeModal = () => {
+  aboutMe.classList.toggle("hideModal");
+  fadeAboutMe.classList.toggle("hideModal");
 };
 
-[openModalButton1, openModalButton2, closeModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
+[openAboutMe, closeAboutMe, fadeAboutMe].forEach((el) => {
+  el.addEventListener("click", () => toggleAboutMeModal());
 });
 
+/* Portfolio | Modal */
+const openPortfolio = document.querySelector("#openPortfolio");
+const closePortfolio = document.querySelector("#closePortfolio");
+const portfolio = document.querySelector("#portfolio");
+const fadePortfolio = document.querySelector("#fadePortfolio");
 
-/* Contact Me */
+const togglePortfolioModal = () => {
+  portfolio.classList.toggle("hideModal");
+  fadePortfolio.classList.toggle("hideModal");
+};
+
+[openPortfolio, closePortfolio, fadePortfolio].forEach((el) => {
+  el.addEventListener("click", () => togglePortfolioModal());
+});
