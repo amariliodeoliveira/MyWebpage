c0146f5bcc922ab77d9b36c825fea0432ecfd00d
Amarilio de Oliveira <amarilioalencar@gmail.com>
2022-10-01 23:33:29 -0300

    modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const openModalButton = document.querySelector("#openAboutMe");
+/* Modal Config */
+const openModalButton1 = document.querySelector("#aboutMeModal");
+const openModalButton2 = document.querySelector("#portfolioModal");
 const closeModalButton = document.querySelector("#closeModal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fadeModal");
@@ -8,6 +10,9 @@ const toggleModal = () => {
   fade.classList.toggle("hideModal");
 };
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
+[openModalButton1, openModalButton2, closeModalButton, fade].forEach((el) => {
   el.addEventListener("click", () => toggleModal());
 });
+
+
+/* Contact Me */
